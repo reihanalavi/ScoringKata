d52f0bc88c07c201e0b8f287271fa8cc6f99a221
reihanalavi <reihanalavi@users.noreply.github.com>
2021-07-07 03:03:47 +0700

    First commit
    Adding all files

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1625601700,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/desimal_-sheet0.png",
+		"images/desimal_-sheet1.png",
+		"images/panahdesimal-sheet0.png",
+		"images/panah-sheet0.png",
+		"images/btnreset-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
